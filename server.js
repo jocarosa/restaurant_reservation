6c8705c6457175e1bb47ab6533b876601db1250a
Joan Carlos Rosario <joancarlosrosarios@hotmail.es>
2016-08-08 20:15:30 +0000

    15-8-2016

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express 	= require('express');
 var routes		= require('./app/routes/index.js');
 var mongoose	= require('mongoose');
 var passport	= require('passport');
-var session 	= require('express-session');
+var session 	= require('express-session')
 
 var app = express();
 require('dotenv').load();
